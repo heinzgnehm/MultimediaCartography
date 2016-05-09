c782fcd028b975d76d3135fa7bce9414f6aa7f53
hgnehm <hgnehm@192.168.15.166>
2016-05-09 17:54:43 +0200

    Created metadata.js with texts for data sets and categories.

---
+++ scripts/metadata.js
@@ -0,0 +1,74 @@
+var metadata = {
+	"homicide_pc": {
+		"title": "Homicide",
+		"unit": "per 100 000 people"
+	},
+	"burglary_pc": {
+		"title": "Burglary",
+		"unit": "per 100 000 people"
+	},
+	"robbery_pc": {
+		"title": "Robbery",
+		"unit": "per 100 000 people"
+	},
+	"battery_pc": {
+		"title": "Battery",
+		"unit": "per 100 000 people"
+	},
+	"theft_pc": {
+		"title": "Theft",
+		"unit": "per 100 000 people"
+	},
+	"assault_pc": {
+		"title": "Assault",
+		"unit": "per 100 000 people"
+	},
+	"narcotics_pc": {
+		"title": "Narcotics",
+		"unit": "per 100 000 people"
+	},
+	"white_p": {
+		"title": "White population",
+		"unit": " %"
+	},
+	"black_p": {
+		"title": "Black population",
+		"unit": " %"
+	},
+	"asian_p": {
+		"title": "Asian population",
+		"unit": " %"
+	},
+	"other_p": {
+		"title": "Other ethnic population",
+		"unit": " %"
+	},
+	"mix_p": {
+		"title": "Mixed ethnic population",
+		"unit": " %"
+	},
+	"hardship": {
+		"title": "Hardship index",
+		"unit": ""
+	},
+	"crowded": {
+		"title": "Crowded housing",
+		"unit": " percent"
+	},
+	"poverty": {
+		"title": "People below poverty level",
+		"unit": " percent"
+	},
+	"income": {
+		"title": "Average income per capita",
+		"unit": " USD"
+	},
+	"diploma": {
+		"title": "Persons without a college degree",
+		"unit": " percent"
+	},
+	"notWorkingAge": {
+		"title": "People aged below 16 and above 65",
+		"unit": " percent"
+	}
+}
